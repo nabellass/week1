c47d512680f3df6574a25fdd0b2fcb097314b78b
nabellass <104151750+nabellass@users.noreply.github.com>
2022-08-04 11:16:24 +0700

    Update horoskop.js

--- horoskop.js
+++ horoskop.js
@@ -1,19 +1,25 @@
-let btn = document.querySelector('button');
-let getOutput = document.querySelector('h3');
+// use if statement
 
-let result = ['Hebat!!', 'Boleh juga!', 'Mengerikan.'];
+const btn = document.getElementById('button');
 
-const randomNumber = Date.now();
-if (randomNumber%3 === 0) {
-  console.log(result[0]);
-}
-if (randomNumber%2 === 0) {
-  console.log(result[1]);
-}
-if (randomNumber%1 === 0) {
-  console.log(result[2]);
-}
+// show result
+btn.addEventListener('click', () => {
+  const getResult = document.createElement('p');
+  const divResult = document.getElementById('result');
 
-btn.addEventListener('click', function() {
-  getOutput.innerText = randomNumber();
+  // get random pick from array
+  const array = [1,1,2,2,2,3,3,3,3,3];
+  const randomArray = array[Math.floor(Math.random()* array.length)];
+  
+  if (randomArray%1 === 0) {
+    getResult.innerHTML = 'Hebat!!';
+  }
+  if (randomArray%2 === 0) {
+    getResult.innerHTML = 'Boleh juga';
+  }
+  if (randomArray%3 === 0) {
+    getResult.innerHTML = 'Tidak beruntung :(';
+  }
+  
+  divResult.append(getResult);
 });
